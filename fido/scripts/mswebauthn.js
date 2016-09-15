80c93d04901159283d0d1f0c12fcff6c5ea0ec18
Rob Trace <Rob.Trace@microsoft.com>
2016-09-15 09:36:56 -0700

    Update for Demo Changes
    This is an update to reflect the following work:
    
    - Convert the demo to use new polyfill from Ade
    - New polyfill uses Index DB instead of Session storage
    - Updated polyfill to use latest public working draft
    
    Remaining work:
    - Fix bugs with get assertion
    - Fix buttons to show correct set based on current machine state
    - Add in feature detection

--- fido/scripts/mswebauthn.js
+++ fido/scripts/mswebauthn.js
@@ -1,91 +1,160 @@
-// This file implements the current FIDO API on top of the Microsoft Edge preliminary implementation.
-// This implementation inherits its limitations on parameter values from the Edge implementation.
-// Initial version: Vijay Bharadwaj, February 2016
+"use strict";
 
-// Notes on limitations:
-// All of this only works if the user has created a PIN (and optionally Hello gestures) for themselves in Settings->Accounts->Sign-in options,
-// as that's what creates the container into which these keys will go
-// Keys will be held in hardware if the machine has a sufficiently performant TPM, otherwise in software
-// makeCredential:
-//    - timeout is ignored
-//    - blacklist is ignored
-//    - all extensions are ignored
-//    - current implementation does not return attestation info
-// getAssertion:
-//    - timeout is ignored
-//    - Whitelist is required in order to get an assertion (this means we can only do U2F-style scenarios, not "typeless" auth where the user is prompted to pick an identity)
-//    - all extensions are ignored except fido.txauth.simple
-//    - signature should be spec compliant if no extension used (signature counter is always zero)
-//    - if txauth.simple extension used, clientData contains an extra field called userPrompt which contains the prompt and authenticatorData still says no extensions
+var WebAuthentication = (function () {
 
-'use strict';
+	const WebAuthenticationDB = (function() {
+		const WebAuthentication_DB_VERSION = 1;
+		const WebAuthentication_DB_NAME = "_WebAuthentication";
+		const WebAuthentication_ID_TABLE = "identities";
 
-var webauthn = (function () {
+		var db = null;
+		var initPromise = null;
 
-    function msMakeCredential(accountInfo, cryptoParams, attestChallenge, timeout, blacklist, ext) {
-		var acct = {rpDisplayName: accountInfo.rpDisplayName, userDisplayName: accountInfo.displayName};
+		function initDB() {
+			return new Promise(function(resolve,reject) {
+				var req = indexedDB.open(WebAuthentication_DB_NAME,WebAuthentication_DB_VERSION);
+				req.onupgradeneeded = function() {
+					// new database - set up store
+					db = req.result;
+					var store = db.createObjectStore(WebAuthentication_ID_TABLE, { keyPath: "id"});
+				};
+				req.onsuccess = function() {
+					db = req.result;
+					resolve();
+				};
+				req.onerror = function(e) {
+					reject(e);
+				};
+			});
+		}
+
+		function store(id,data) {
+			if(!initPromise) { initPromise = initDB(); }
+			return initPromise.then(function() { doStore(id,data) });
+		}
+
+		function doStore(id,data) {
+			if(!db) throw "DB not initialised";
+			return new Promise(function(resolve,reject) {
+				var tx = db.transaction(WebAuthentication_ID_TABLE,"readwrite");
+				var store = tx.objectStore(WebAuthentication_ID_TABLE);
+				store.put({id:id,data:data});
+				tx.oncomplete = function() {
+					resolve();
+				}
+				tx.onerror = function(e) {
+					reject(e);
+				};
+			});
+		}
+
+		function getAll() {
+			if(!initPromise) { initPromise = initDB(); }
+			return initPromise.then(doGetAll);
+		}
+
+		function doGetAll() {
+			if(!db) throw "DB not initialized";
+			return new Promise(function(resolve,reject) {
+				var tx = db.transaction(WebAuthentication_ID_TABLE,"readonly");
+				var store = tx.objectStore(WebAuthentication_ID_TABLE);
+				var req = store.openCursor();
+				var res = [];
+				req.onsuccess = function() {
+					var cur = req.result;
+					if(cur) {
+						res.push({id:cur.value.id,data:cur.value.data});
+						cur.continue();
+					} else {
+						resolve(res);
+					}
+				}
+				req.onerror = function(e) {
+					reject(e);
+				};
+			});
+		}
+
+		return {
+			store: store,
+			getAll: getAll
+		};
+	}());
+
+    function makeCredential(accountInformation, cryptoParameters, attestationChallenge, options) {
+		var acct = {rpDisplayName: accountInformation.rpDisplayName, userDisplayName: accountInformation.displayName};
 		var params = [];
 		var i;
 		
-		if (accountInfo.name) { acct.accountName = accountInfo.name; }
-		if (accountInfo.id) { acct.userId = accountInfo.id; }
-		if (accountInfo.imageUri) { acct.accountImageUri = accountInfo.imageUri; }
+		if (accountInformation.name) { acct.accountName = accountInformation.name; }
+		if (accountInformation.id) { acct.userId = accountInformation.id; }
+		if (accountInformation.imageUri) { acct.accountImageUri = accountInformation.imageUri; }
 
-		for ( i = 0; i < cryptoParams.length; i++ ) {
-			if ( cryptoParams[i].type === 'FIDO' ) {
-				params[i] = { type: 'FIDO_2_0', algorithm: cryptoParams[i].algorithm };
+		for ( i = 0; i < cryptoParameters.length; i++ ) {
+			if ( cryptoParameters[i].type === 'ScopedCred' ) {
+				params[i] = { type: 'FIDO_2_0', algorithm: cryptoParameters[i].algorithm };
 			} else {
-				params[i] = cryptoParams[i];
+				params[i] = cryptoParameters[i];
 			}
 		}
-        return msCredentials.makeCredential(acct, params, attestChallenge).then(function (cred) {
-			if (cred.type === 'FIDO_2_0'){
-				return Object.freeze({
-					credential: {type: 'FIDO', id: cred.id},
-					algorithm: cred.algorithm,
+        return msCredentials.makeCredential(acct, params, attestationChallenge).then(function (cred) {
+			if (cred.type === "FIDO_2_0") {
+				var result = Object.freeze({
+					credential: {type: "ScopedCred", id: cred.id},
 					publicKey: JSON.parse(cred.publicKey),
 					attestation: cred.attestation
 				});
+				return WebAuthenticationDB.store(result.credential.id,accountInformation).then(function() { return result; });
 			} else {
 				return cred;
 			}
 		});
     }
 
-    function msGetAssertion(challenge, timeout, whitelist, ext) {
-		var filter = undefined;
+    function getCredList(whitelist) {
 		var credList = [];
-		var sigParams = undefined;
-		var j;
-        
-		if (whitelist) { 
-			for ( j = 0; j < whitelist.length; j++ ) {
-				if ( whitelist[j].type === 'FIDO' ) {
-					credList[j] = { type: 'FIDO_2_0', id: whitelist[j].id };
-				} else {
-					credList[j] = whitelist[j];
-				}
-			}
-			filter = { accept: credList }; 
-		}
-		if (ext['fido.txauth.simple']) { sigParams = { userPrompt: ext['fido.txauth.simple'] }; }
+    	if(whitelist) {
+    		return new Promise(function(resolve,reject) {
+    			whitelist.forEach(function(item) {
+					if (item.type === 'ScopedCred' ) {
+						credList.push({ type: 'FIDO_2_0', id: item.id });
+					} else {
+						credList.push(item);
+					}
+    			});
+    			resolve(credList);
+			});
+    	} else {
+    		return WebAuthenticationDB.getAll().then(function(list) {
+    			list.forEach(item => credList.push({ type: 'FIDO_2_0', id: item.id }));
+    			return credList;
+    		});
+    	}
+    }
 
-        return msCredentials.getAssertion(challenge, filter, sigParams).then(function (sig) {
-			if (sig.type === 'FIDO_2_0'){
-				return Object.freeze({
-					credential: {type: 'FIDO', id: sig.id},
-					clientData: sig.signature.clientData,
-					authenticatorData: sig.signature.authnrData,
-					signature: sig.signature.signature
-				});
-			} else {
-				return sig;
-			}
+    function getAssertion(assertionChallenge, options) {
+		return getCredList(options.allowList).then(function(credList) {
+			var filter = { accept: credList }; 
+			var sigParams = undefined;
+			if (credentialExtensions && credentialExtensions["WebAuthentication.txauth.simple"]) { sigParams = { userPrompt: credentialExtensions["WebAuthentication.txauth.simple"] }; }
+
+	        return msCredentials.getAssertion(assertionChallenge, filter, sigParams).then(function (sig) {
+				if (sig.type === "FIDO_2_0"){
+					return Object.freeze({
+						credential: {type: "ScopedCred", id: sig.id},
+						clientData: sig.signature.clientData,
+						authenticatorData: sig.signature.authnrData,
+						signature: sig.signature.signature
+					});
+				} else {
+					return sig;
+				}
+			});
 		});
     }
 
     return {
-        makeCredential: msMakeCredential,
-        getAssertion: msGetAssertion
+        makeCredential: makeCredential,
+        getAssertion: getAssertion
     };
-}());
+})();
